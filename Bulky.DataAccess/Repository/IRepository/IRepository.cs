d10d89b4b4c7ac4d7e41d0d818893136295c0d9d
OYaghi <OYaghi@DESKTOP-14L6TL7>
2024-07-06 11:02:24 +0300

    Adding Floating Input

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    internal interface IRepository<T> where T: class
+    {
+
+    }
+}
